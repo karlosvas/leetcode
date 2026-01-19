9a3140fd8cec9e0f7015d7e057032a1b7416b1b4
karlosvas <carlosvassan@gmail.com>
2026-01-19 16:32:54 +0100

    Solve problem 100: Same Tree

---
+++ src/easy/100_same_tree.rs
@@ -0,0 +1,73 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::easy::Solution;
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        // 1,2,3
+        let mut stack: Vec<(Option<Rc<RefCell<TreeNode>>>, Option<Rc<RefCell<TreeNode>>>)> =
+            vec![(p, q)];
+
+        // Recorremos todos los pendientes
+        while let Some((node_p, node_q)) = stack.pop() {
+            match (node_p, node_q) {
+                (Some(l), Some(r)) => {
+                    if l.borrow().val != r.borrow().val {
+                        return false;
+                    }
+
+                    stack.push((l.borrow().left.clone(), r.borrow().left.clone()));
+                    stack.push((l.borrow().right.clone(), r.borrow().right.clone()));
+                }
+                (None, None) => {}
+                _ => return false,
+            }
+        }
+
+        true
+    }
+}
+
+// 100. Same Tree
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+// Example 1:
+
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+// Example 2:
+
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+// Example 3:
+
+// Input: p = [1,2,1], q = [1,1,2]
+// Output: false
+
+// Constraints:
+
+// The number of nodes in both trees is in the range [0, 100].
+// -104 <= Node.val <= 104
